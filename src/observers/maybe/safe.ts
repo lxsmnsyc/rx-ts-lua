b15f5fa5443c6cd7092512ad66162b8d82d50827
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-02-01 17:56:30 +0800

    Create safe.ts

---
+++ src/observers/maybe/safe.ts
@@ -0,0 +1,87 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2020 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2020
+ */
+import { MaybeObserver } from '../../types/observers';
+import Subscription from '../../types/subscription';
+import { LivingObject } from '../../types/utils';
+import LivingSubscription from '../../subscriptions/living';
+
+export default class SafeMaybeObserver<T> implements MaybeObserver<T>, LivingObject {
+  private subscription?: Subscription;
+
+  public alive = true;
+
+  private upstream: MaybeObserver<T>;
+
+  constructor(observer: MaybeObserver<T>) {
+    this.upstream = observer;
+  }
+
+  public onSubscribe(subscription: Subscription): void {
+    if (this.alive) {
+      this.subscription = new LivingSubscription(this, subscription);
+    } else {
+      subscription.cancel();
+    }
+  }
+
+  public onComplete(): void {
+    if (this.alive) {
+      try {
+        this.upstream.onComplete();
+      } finally {
+        this.cancel();
+      }
+    }
+  }
+
+  public onSuccess(value: T): void {
+    if (this.alive) {
+      try {
+        this.upstream.onSuccess(value);
+      } finally {
+        this.cancel();
+      }
+    }
+  }
+
+  public onError(error: any): void {
+    if (this.alive) {
+      try {
+        this.upstream.onError(error);
+      } finally {
+        this.cancel();
+      }
+    }
+  }
+
+  private cancel(): void {
+    if (this.subscription) {
+      this.subscription.cancel();
+    }
+  }
+}
