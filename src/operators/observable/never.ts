646f84543471db5e3c29ed8d44a03bba656c65c8
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-02-02 17:05:33 +0800

    Added never operators

---
+++ src/operators/observable/never.ts
@@ -0,0 +1,43 @@
+/**
+ * @license
+ * MIT License
+ *
+ * Copyright (c) 2020 Alexis Munsayac
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *
+ * @author Alexis Munsayac <alexis.munsayac@gmail.com>
+ * @copyright Alexis Munsayac 2020
+ */
+import Observable from '../../observable';
+import { ObservableObserver } from '../../types/observers';
+import SafeObservableObserver from '../../observers/observable/safe';
+import EMPTY_SUBSCRIPTION from '../../subscriptions/empty';
+
+class ObservableNever<T> extends Observable<T> {
+  protected subscribeActual(observer: ObservableObserver<T>): void {
+    const newObserver = new SafeObservableObserver(observer);
+
+    newObserver.onSubscribe(EMPTY_SUBSCRIPTION);
+  }
+}
+
+export default function never<T>(this: void): Observable<T> {
+  return new ObservableNever<T>();
+}
