837a77807279ef188ad0bb55fb5a11068bdbf203
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-02-02 17:06:30 +0800

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,11 +1,19 @@
 
 local rx = require('init');
 
-local single = rx.operators.single.just('Hello World')
+
+local single = rx.operators.single.just('Hello'):pipe(
+  rx.operators.single.map(function (value)
+    return value.." World"
+  end),
+  rx.operators.single.map(function (value)
+    return 'Success: '..value
+  end)
+)
 
 single:subscribe(
   function (value)
-    print("Success: "..value)
+    print(value)
   end,
   function (err)
     print("Error: "..err)
